d30d6a5a1b71342c3854110673e0f29dced9ae51
Kathleen Moran <kathleenmoran10@gmail.com>
2022-05-02 17:07:34 -0400

    Add functionality to print board

--- board.rb
+++ board.rb
@@ -4,7 +4,35 @@ class Board
   DIMENSION = 3
   def initialize
     @values = Array.new(DIMENSION){Array.new(DIMENSION)}
-    @player_1 = Player.new("X", self)
-    @player_2 = Player.new("O", self)
+    @player_1 = Player.new("X")
+    @player_2 = Player.new("O")
+    puts self
+  end
+
+  private
+  def to_s
+    board_as_string = "\n"
+    @values.each_with_index do |row, index|
+      board_as_string += row_to_s(row, index)
+      if index < DIMENSION - 1
+        board_as_string += "---+---+---" # horizontal row boarder
+      end
+      board_as_string += "\n"
+    end
+    board_as_string
+  end
+
+  private
+  def row_to_s(row, row_index)
+    row_as_string = ""
+    row.each_with_index do |square, square_index|
+      row_as_string += square ? " #{square} " : " #{square_index + row_index * 3 + 1} "
+      if square_index < DIMENSION - 1
+        row_as_string += "|" # vertical square boarder
+      else 
+        row_as_string += "\n"
+      end
+    end
+    row_as_string
   end
 end
--- player.rb
+++ player.rb
@@ -2,15 +2,27 @@ require_relative 'board'
 
 class Player
   @@total_players = 0
-  def initialize(piece, board)
+  def initialize(piece)
     @name = input_name
     @piece = piece
-    @board = board
     @@total_players += 1
+    puts self
   end
 
+  private
   def input_name
     puts "What is the name of player #{@@total_players + 1}?"
     gets.chomp
   end 
+
+  private
+  def to_s
+    "#{@name}, your piece is '#{@piece}'."
+  end
+
+  private
+  def input_move
+    puts "#{@name}, please enter a number (1-9) that is available to place a '#{piece}'."
+    gets.chomp.to_i - 1
+  end
 end
